461b8002491f18f101e672416fa7ae96002c6986
Emily Dokken and Jessica Robertson <jessica@>
2017-03-06 18:51:01 -0800

    add all inital css js and html files as well as create initial jquery rule for our leap year program to correspond with the form on index.html.

---
+++ js/scripts.js
@@ -0,0 +1,19 @@
+//Business Logic
+
+var leapYear = function(year) {
+  return false;
+};
+
+
+//User Interface Logic
+
+$(document).ready(function() {
+  $("form#leap-year").submit(function(event) {
+    event.preventDefault();
+    var year = parseInt($("input#year").val());
+    var result = leapYear(year);
+    $("#result").text(result);
+  });
+
+
+});
